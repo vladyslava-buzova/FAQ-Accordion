399db4bd4fcb1cb80d6b00012f4660586b17702d
Vladyslava Buzova <dev.vladyslava@gmail.com>
2022-08-04 23:19:33 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// select all the buttons
+
+const btns = document.querySelectorAll(".question-btn");
+
+// click on the "plus" button => toggle(add/remove) class .show-text
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const questions = e.currentTarget.parentElement.parentElement;
+    questions.classList.toggle("show-text");
+    })
+});
+
+
